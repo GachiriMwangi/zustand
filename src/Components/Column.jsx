a9d691845fb2ab7303bbf9997d897e42da7022b2
GachiriMwangi <gachirimwangi24@gmail.com>
2024-06-10 12:28:17 +0300

    initial commit

---
+++ src/Components/Column.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./column.css"
+const Column = ({state}) => {
+  return (
+    <div className="column">
+      {state}
+    </div> 
+  )
+}
+
+export default Column
